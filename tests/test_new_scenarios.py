5853773a77868fdc6f111bf9f5bf01cbadd0aaed
a.ostaenkov <a.ostaenkov@okko.tv>
2021-10-26 23:42:15 +0500

    Remove tests with broken auth

--- tests/test_new_scenarios.py
+++ tests/test_new_scenarios.py
@@ -25,31 +25,3 @@ def test_switch_currency_to_pound(driver):
     HeaderElement(driver).click_on_pound()
     text = BasketElement(driver).get_basket_text()
     assert "£" in text, 'Text message not equal to expected'
-
-
-@allure.title("Добавление нового продукта в каталог")
-def test_add_new_product_to_catalog(driver):
-    """
-    By default Opencart use predefined password for admin user therefore this test failure
-    """
-    admin_page = AdminPage(driver)
-    admin_page.login()
-    admin_page.open_products()
-    admin_page.add_new_product()
-    admin_page.fill_product_card(product_name='Testing product_' + helpers.get_timestamp(),
-                                 tag_title='Testing tag',
-                                 product_model='Testing model')
-    text = SuccessAlert(driver).get_text_from_alert()
-    assert 'Success: You have modified products!' in text, 'Text message not equal to expected'
-
-
-@allure.title("Удаление продукта из каталога")
-def test_delete_product_from_catalog(driver):
-    admin_page = AdminPage(driver)
-    admin_page.login()
-    admin_page.open_products()
-    admin_page.select_product_from_catalog()
-    admin_page.delete_product()
-    admin_page.accept_alert()
-    text = SuccessAlert(driver).get_text_from_alert()
-    assert 'Success: You have modified products!' in text, 'Text message not equal to expected'
